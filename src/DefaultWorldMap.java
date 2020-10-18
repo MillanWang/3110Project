e9afbacd370128fe06c467266e267488074f7d50
Millan <real.millan.wang@gmail.com>
2020-10-17 23:12:04 -0400

    Testing to add first file. Completely incomplete

---
+++ src/DefaultWorldMap.java
@@ -0,0 +1,14 @@
+public class DefaultWorldMap {
+    /**
+     * This class is to represent the entire playing map in the game RISK for
+     * the group project in SYSC 3110
+     *
+     * Group: Static
+     * Members: J.Anyia, F.Olotu, T.Shabaan, M.Wang
+     * */
+    private String testFieldForGit;
+
+    public DefaultWorldMap(){
+        //Should set up the whole world :)
+    }
+}
