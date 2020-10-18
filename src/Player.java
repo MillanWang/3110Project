cdc2097b04996fbd5b1e5b4244fbb795f6a5a247
Favourolotu <72259260+Favourolotu@users.noreply.github.com>
2020-10-18 18:18:56 -0400

    Create Player.java

---
+++ src/Player.java
@@ -0,0 +1,30 @@
+/**
+ * A class representing a player in the game.
+ */
+public class Player {
+    private String name; // The name of the player.
+    private ArrayList<Teritory> teritories; // A list of the teritories the player occupies.
+
+    /**
+     * A constructor that sets the player class variables.
+     */
+    public Player(String name) {
+        this.name = name;
+        clearTeritory();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Clears the countries list.
+     */
+    public void clearTeritory() {
+        territories = new ArrayList<>();
+    }
+
+
+    public void removeTerritory(Teritory teritory) {
+        teritories.remove(teritory);
+    }
