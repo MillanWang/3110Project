1e456cc79ac8496a457f691dbf3d8bdb4e3462db
Favourolotu <72259260+Favourolotu@users.noreply.github.com>
2020-10-19 01:31:41 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,21 @@
+import java.util.ArrayList;
+
 /**
  * A class representing a player in the game.
  */
 public class Player {
     private String name; // The name of the player.
-    private ArrayList<Teritory> teritories; // A list of the teritories the player occupies.
+    private ArrayList<Teritory> territories; // A list of the territories the player occupies.
+    private int troops; // number of troops a player has
     
-//Mention how many troops does each player have
-//Mention the bonus troops too    
+//Mention the bonus troops too
+
     /**
      * A constructor that sets the player class variables.
      */
     public Player(String name) {
         this.name = name;
-        clearTeritory();
+        clearTerritory();
     }
 
     public String getName() {
@@ -22,11 +25,22 @@ public class Player {
     /**
      * Clears the countries list.
      */
-    public void clearTeritory() {
+    public void clearTerritory() {
         territories = new ArrayList<>();
     }
 
 
     public void removeTerritory(Teritory teritory) {
-        teritories.remove(teritory);
+        territories.remove(teritory);
     }
+
+    public int getTroops() {
+        return troops;
+    }
+    
+    public void bonusTroops(int bonus){
+        if (true){ // will be updated once we review the condition for adding bonus troops
+            troops +=  bonus;
+            }
+    }
+}
