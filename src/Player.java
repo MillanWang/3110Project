e390b10b37c644a25293060ead952c5a4dad1816
Favourolotu <72259260+Favourolotu@users.noreply.github.com>
2020-10-21 01:08:16 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,41 +1,93 @@
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * A class representing a player in the game.
  */
 public class Player {
     private String name; // The name of the player.
-    private ArrayList<Teritory> territories; // A list of the territories the player occupies.
-    private int troops; // number of troops a player has
-    
-//Mention the bonus troops too
+    private List<Teritory> territories; // A list of the territories the player occupies.
+    private final Map<String, Integer> continentBonuses= new HashMap<String, Integer>() {{
+        put("Africa", 3);
+        put("Asia", 7);
+        put("Europe", 5);
+        put("North America", 5);
+        put("Oceania", 2);
+        put("South America", 2);
+    }}; // A map with the troop bonuses for every continent, by its name.
+
 
     /**
      * A constructor that sets the player class variables.
+     * @ param String  the name of the player
      */
     public Player(String name) {
         this.name = name;
         territories = new ArrayList<>();
     }
 
+    /**
+     * this method returns the name of the player
+     * @ return  name players name
+     */
     public String getName() {
         return this.name;
     }
 
- 
-
-
+    /**
+     * this method removes the a territory from the list of territories owned by the player
+     * @ param territory the territory to be removed
+     **/
     public void removeTerritory(Teritory teritory) {
-        territories.remove(teritory);
+        if (!(hasLost())) {
+            territories.remove(teritory);
+            if(hasLost()){
+                handleDeadPlayer();
+            }
+        }else{
+            handleDeadPlayer();
+        }
     }
 
-    public int getTroops() {
-        return troops;
+    /**
+     * @return Whether the player has lost all territories.
+     */
+    private  boolean hasLost() {
+        return territories.isEmpty();
     }
-    
-    public void bonusTroops(int bonus){
-        if (true){ // will be updated once we review the condition for adding bonus troops
-            troops +=  bonus;
-            }
+
+    /**
+     * this method handles the dead player state
+     */
+    private  void handleDeadPlayer() {
+        // additional implementation may be needed.
+        System.out.println("THE KING IS DEAD");
     }
+
+
+
+    /**
+     * this method handles the bonus troops stage of the risk game from owning all territories in a continent
+     * @ param bonus the amount of bonus troops player received
+     */
+    public int bonusTroops(String cName){ 
+        // i need more information to complete this
+        return 1;
+    }
+
+    /**
+     * Calculates the number of troops the player should receive in this turn.
+     * It is based on the number of countries he occupies and any continents he fully controls.
+     * @param continents The list of continents in the game
+     * @return The number of new troops the player should receive in this turn.
+     */
+    public int getBonusTroops(Collection<Continent> continents) {
+        int continentBonuses = 0;
+        for (Continent continent : continents) {
+            if (this.territories.containsAll(continent.getTerritories()))
+                continentBonuses += bonusTroops(continent.getName());
+
+        }
+        return this.territories.size() / 3 + continentBonuses;
+    }
+
 }
