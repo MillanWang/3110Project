ae473ab7fa10a3e675d46ddbfc3a57745d451c4d
Taher Shabaan <55097549+TaherShabaan99@users.noreply.github.com>
2020-10-25 17:36:10 -0400

    Update Territory.java

--- src/Territory.java
+++ src/Territory.java
@@ -1,51 +1,56 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Territory class holds each territory name, how many troops on it, who owns it and it's neighbours.
+ */
 public class Territory {
-    private int troops;
-    private String territoryName;
-    private String owner;
-    private List<Territory> neighboursList;
+    private int troops;//troops and armies are the same thing
+    private String territoryName;// the name of the territory
+    private String owner;// the name of the player who owns this territory
+    private List<Territory> neighboursList;//each territory should know it's neighbours
 
-
+    /**
+     * Initializing the class states in this constructor
+     * @param territoryName
+     */
     public Territory(String territoryName){
         this.territoryName = territoryName;
         this.troops = 0;
         this.neighboursList = new ArrayList<>();
     }
 
-
     /**
+     * this method would return the number of troops owned by this territory
      * @return troops
      */
     public int getTroops() {
         return troops;
     }
 
+    /**
+     * this method would set the number of troops owned by this territory
+     * @param troops
+     */
     public void setTroops(int troops){
         this.troops = troops;
     }
 
     /**
      * @param troops
-     * troops could be positive or negative
+     * the number of troops that is placed on each territory
+     * would change in the draft and attack phase
      */
     public void changeTroops(int troops) {
         this.troops += troops;
     }
 
     /**
-     * @return territory name
+     * get the territory name
+     * @return territoryName
      */
     public String getTerritoryName() {
         return territoryName;
-    }
-
-    /**
-     * @param territoryName
-     */
-    public void setTerritoryName(String territoryName) {
-        this.territoryName = territoryName;
     }
 
     /**
@@ -56,6 +61,7 @@ public class Territory {
     }
 
     /**
+     * setting the name of the owner
      * @param owner
      */
     public void setOwner(String owner) {
@@ -63,39 +69,43 @@ public class Territory {
     }
 
     /**
-     *
-     * @param
-     * @return a neighbour in the neighbour list
+     * @param territoryName
+     * @return a territory object that
+     * it's name is equal to territoryName
      */
     public Territory getNeighbour(String territoryName) {
-        for (Territory t : neighboursList){
-            if (territoryName.equals(t.getTerritoryName())){
-                return t;
+        for (Territory territory : neighboursList){
+            if (territoryName.equals(territory.getTerritoryName())){
+                return territory;
             }
         }
         System.out.println("No territory with this name");
-        return null; //To compile
+        return null;
     }
 
     /**
-     *
+     * adding a new territory neighbour to the neighboursList
      * @param newNeighbours
      */
-    //NAME CHANGED. NOT ADDING A LIST, ADDING AN INDIVIDUAL NEIGHBOUR
     public void addNeighbours(Territory newNeighbours) {
         this.neighboursList.add(newNeighbours);
     }
 
+    /**
+     * this method would print out each territory name with
+     * the number of troops and who owns it
+     */
     public void printInfo(){
         StringBuilder sb = new StringBuilder(territoryName);
         sb.append("                                                                     ");
         sb.insert(25,"\tTroops: " + troops + "\t\tOwner: " + owner );
         System.out.println(sb);
     }
+
     /**
-     *Favour
-     * This method should get the a list of neighbours owner can attack
-     * */
+     * This method should return a list of territories that can be attacked
+     * @return a list of territories
+     */
     public List<Territory> getAttackableNeighbours(){
         if(this.getTroops() <= 1 ) return null; //Cannot start attack from terry with 1 troop
 
@@ -111,12 +121,23 @@ public class Territory {
             return attackableNeighbours;
         }
     }
+
+    /**
+     * this method print the information of each
+     * possible attackable neighbours (territory)
+     */
     public void printAttackableNeighbours(){
         for (Territory territory:getAttackableNeighbours()){
             territory.printInfo();
         }
     }
 
+    /**
+     * @param defender
+     * @return true or false depends on the situation
+     * true: in case the player can attack that territory
+     * false: in case the player cannot attack that territory
+     */
     public boolean canAttack(String defender){
         List<Territory> attackables = getAttackableNeighbours();
         for(Territory territory: attackables){
@@ -126,5 +147,4 @@ public class Territory {
         }
         return false;
     }
-
 }
