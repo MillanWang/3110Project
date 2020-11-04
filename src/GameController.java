607843bb0006cca0190eb8553988b63d834f1e3e
Favourolotu <72259260+Favourolotu@users.noreply.github.com>
2020-11-03 23:54:09 -0500

    Update GameController.java

--- src/GameController.java
+++ src/GameController.java
@@ -1,8 +1,23 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GameController {
     private Game game;
+    private GameView gameView;
 
-    public GameController(Game game){
+    public GameController(Game game,  GameView gameView) {
         this.game = game;
+        this.gameView = gameView;
+        this.gameView.addGameListner(new GameListner());
+    }
+
+    class GameListner implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Player player;
+            
+        }
     }
 
 }
