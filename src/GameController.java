aab551b2a31b91394775fb6a5bc588c42efd90b7
MillanWang <72239525+MillanWang@users.noreply.github.com>
2020-11-04 23:06:04 -0500

    Update GameController.java

--- src/GameController.java
+++ src/GameController.java
@@ -1,28 +1,23 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
 
-public class GameController {
+public class GameController implements ActionListener{
     private Game game;
     private GameView gameView;
 
     public GameController(Game game,  GameView gameView) {
         this.game = game;
         this.gameView = gameView;
-        this.gameView.addGameListner(new GameListner());
     }
 
-    class GameListner implements ActionListener{
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            Player player;
-            
-        }
-    }
-    
     public LinkedList<Territory> getPlayersTerritoriesForDraft(){
         return game.getCurrentPlayerObject().getTerritoriesList();
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
