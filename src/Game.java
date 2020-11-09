bfb92a04588371e0434617d7769a26d3ccbf519b
Favourolotu <72259260+Favourolotu@users.noreply.github.com>
2020-11-08 22:55:10 -0500

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,26 +1,18 @@
 import java.util.*;
 
 public class Game {
-    private Parser parser;
     private LinkedList<Player> players;
     private Player currentPlayer;
     private Dice dice;
-
-
-
     private GenericWorldMap genericWorldMap;
-    private boolean gameEnds;
 
     /**
      * Constructor for the class game. This will start playing the game
      */
     public Game(){
         genericWorldMap = new GenericWorldMap();
-        parser = new Parser();
         players = new LinkedList<Player>();
-        gameEnds = false;
         dice = new Dice();
-        //play(); no need for it anymore
     }
 
     /**
@@ -53,20 +45,7 @@ public class Game {
         return gameRules;
     }
 
-    /**
-     * Print out the legal commands of the game for the user
-     */
-    private void printHelp()
-    {
-        System.out.println("******************************");
-        System.out.println("Make sure to choose from the following commands.");
-        System.out.println("showMap : Prints a list of all territories on the map with number of troops on them and owner");
-        System.out.println("nextTurn : Starts the Draft>Attack turn cycle for the next player");
-        System.out.println("quit : To end the game");
-        System.out.println("help : To show this message\n");
-        System.out.println("Your command words are:");
-        System.out.println("showMap, nextTurn ,quit ,help");
-    }
+
     /**
      * Return a message to the user if they want to quit.
      *
@@ -76,30 +55,38 @@ public class Game {
         return "Thanks for playing. Goodbye";
     }
 
-    /**
-     * Starts the game. The game will end when the player chooses to quit or when a winner is found
-     */
-    private void play(){
-        welcomeMessage();
-        System.out.println("******************************");
-        //makePlayers();// It'll be called by GameView
 
-        while (!gameEnds){
-            Command command = parser.getCommand();
-            gameEnds = processCommand(command);
-        }
-    }
+    /**
+     * @return the name of the current player
+     *
+     */
     public String getCurrentPlayer(){
         return this.currentPlayer.getName();
     }
+
+    /**
+     * @return the player object of the current player
+     *
+     */
     public Player getCurrentPlayerObject(){
         return this.currentPlayer;
     }
 
+    /**
+     * @return a linked list of the all the players not including the current player
+     *
+     */
     public LinkedList<Player> getPlayersList(){
         return players;
     }
 
+    /**
+     * This method returns a reference to the the territory input
+     *
+     * @param territoryName name of the territory
+     * @return the name of the current player
+     *
+     */
     public Territory getTerritory(String territoryName){
         return genericWorldMap.getTerritory(territoryName);
     }
@@ -113,160 +100,14 @@ public class Game {
     }
 
     /**
-     * Processes the commands from the user in the main menu interface section
-     * @param command The command given by the player
-     * @return True if the player wants to end the game. False otherwise
-     */
-    private boolean processCommand(Command command) {
-        boolean wantToQuit = false;
-        if (command.isUnknown()) {
-            System.out.println("I don't know what you mean...");
-            return false;
-        } else if (command.getCommandWord().equals("help")) {
-            printHelp();
-        }else if (command.getCommandWord().equals("quit")){
-            System.out.println("Thank you for playing RISK\nHave a nice day!!");
-            wantToQuit = true;
-        }else if (command.getCommandWord().equals("nextTurn")){
-            nextTurn();
-        }else if (command.getCommandWord().equals("showMap")) {
-            genericWorldMap.printAllTerritories();
-            printHelp();
-        }
-        return wantToQuit;
-    }
-
+     * @return the true if the game has a winner
+     * false if there is no winner yet
+     *
+    */
     public boolean hasWinner(){
         return players.isEmpty();
     }
 
-    /**
-     * The attack phase of a player's turn
-     * If player has territories that can start an attack, the player has the option to choose if they want to
-     * start the attack or move on to the next phase (ends turn for now, fortify stage in the future)
-     *
-     * If a player decides to start an attack, ask where to start the attack
-     * Once attack starter is determined, choose where to attack (the defender)
-     *
-     * Initiates a diceFight process (Dice class)
-     * This may lead to a territory being conquered by the attacker. This would be a call to takeoverTerritory
-     *
-     * The attack phase is forced to end if the current player no longer has any territories that can start an attack
-     */
-
-    private void attackPhase() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("******************************");
-        System.out.println("Starting attack phase of turn for player " + currentPlayer.getName());
-        System.out.println("******************************");
-        //currentPlayer.printAttackStarters();
-
-        Territory attackStarterTerritory, defenderTerritory;// Needed for diceFight
-        boolean endAttack = false;
-
-        while(!endAttack){
-            System.out.println("Type \"attack\" to start an attack or type \"skip\" to move on");
-            //Ask player to start attack or skip attack phase and move on to (NEXT TURN). Verify input
-            String attackOrSkip = "";
-            if (currentPlayer.getAttackStarters().length == 0) {
-                endAttack = true;
-                System.out.println("No territories can start an attack");
-                break;
-            }else {
-                attackOrSkip = input.nextLine();
-            }
-
-            if (attackOrSkip.equals("skip")) {
-                endAttack = true;
-                System.out.println("Ending attack phase of turn. Moving on...");
-
-
-                //AttackChoiceStage begins here
-            } else if (attackOrSkip.equals("attack") ){
-                System.out.println("Starting attack");
-                //Player wants to attack. Give options of attack starters
-
-                //Ask for attack starter. Verify it is legit
-                while (true){
-                    System.out.println("\nYou are currently able to start an attack from the following territories" );
-                    //currentPlayer.printAttackStarters(); //shows territories current player is neighbours with
-                    System.out.println("Choose from above territories where to start an attack from ");
-
-                    String attackStarter = input.nextLine();
-
-                    if (currentPlayer.getTerritory(attackStarter)==null){
-                        System.out.println("Territory name does not exist. Try again.");
-                    } else if (currentPlayer.canStartAttack(attackStarter)){
-                        //Start attack
-                        attackStarterTerritory = currentPlayer.getTerritory(attackStarter);
-                        System.out.println("Starting attack from " + attackStarter);
-                        break;
-                    } else {
-                        System.out.println("Cannot start an attack from that territory");
-                    }
-                }//At this point we have a pointer to the attackStarterTerritory
-                List<Territory> attackableNeighbours = attackStarterTerritory.getAttackableNeighbours();
-
-                while (true){
-                    System.out.println("\nYou are currently able to attack the following territories" );
-                    attackStarterTerritory.printAttackableNeighbours(); //shows territories that the current player can attack
-
-                    String defender = input.nextLine();
-
-                    //Get input from the user
-                    //Check if input corresponds to a name of a terry that is attackable from attackStarterTerritory
-
-                    if (attackStarterTerritory.canAttack(defender)){
-                        //Valid defender is chosen
-                        defenderTerritory = attackStarterTerritory.getNeighbour(defender);
-                        break;
-                    } else {
-                        System.out.println("Invalid defender chosen. Try again!");
-                    }
-                }
-                //Dice fight stage
-                while(true){
-                    System.out.print("******************************\nAttack starting from: ");
-                    attackStarterTerritory.printInfo();
-                    System.out.print("\nAttacking: ");
-                    defenderTerritory.printInfo();
-
-                    System.out.println("Type \"dice\" to start a battle or \"endBattle\" to end a battle");
-                    String diceFightChoice = input.nextLine();
-
-                    if (diceFightChoice.equals("dice")){
-                        //int diceWonWith = diceFight(attackStarterTerritory,defenderTerritory, attackStarterTerritory.maxDiceToRoll());
-
-
-                        /**
-                         *
-                         *
-                         * PROBABLY IMPORTANT
-                         *
-                         *
-                         */
-
-                        if (defenderTerritory.getTroops() <=0 ){
-                            //Defender has been killed
-                            takeoverTerritory(currentPlayer,attackStarterTerritory, defenderTerritory, 3);//diceWonWith); Controller should know this
-                            break;
-                        } else if ( attackStarterTerritory.getTroops() <= 1){
-                            //Attacker can no longer attack from this territory
-                            System.out.println("Can no longer attack from " + attackStarterTerritory.getTerritoryName());
-                            System.out.println("Only one troop left!");
-                            break;
-                        }
-                    } else if (diceFightChoice.equals("endBattle")){
-                        break;
-                    } else {
-                        System.out.println("Unknown command. Try again!");
-                    }
-                }
-            } else {
-                System.out.println("Invalid command!");
-            }//End check for valid command
-        }//End while(!endAttack)
-    }//End attackPhase()
 
     /**
      * During a player's attack phase, after the attack starter and the defender are chosen,
@@ -328,6 +169,13 @@ public class Game {
         return diceFightResultMessage;
     }
 
+    /**
+     * this method returns a string representation of the result of the dice fight
+     *
+     * @param attackerDefender a string array of the draft result
+     * @return string representation of the result of the dice fight
+     *
+     */
     public String diceFightInfo(String[] attackerDefender){
         return genericWorldMap.getTerritory(attackerDefender[0]).getInfoString() + "\n" + genericWorldMap.getTerritory(attackerDefender[1]).getInfoString();
     }
@@ -364,31 +212,15 @@ public class Game {
         return false;
     }
 
-
-
-    /**\
+    /**
      * Eliminates player from the game given pointer to their player object
      *
      * @param loser The player eliminated from the game
      */
     private void eliminatePlayer(Player loser){
         players.remove(loser);
-        System.out.println("Rest in peace "+ loser.getName() + ", you have been eliminated");
-        if (players.isEmpty()){
-            System.out.println(currentPlayer.getName()+ " is the ULTIMATE RULER OF THE WORLD!!!");
-            gameEnds = true;
-        }
     }
 
-
-    //OLD DOCUMENTATION FOR makePlayers()
-    /**
-     * Asks for how many players this game will have and verifies that it is 2-6
-     * Then asks user to choose unique player names for each player
-     * Randomly distributes the territories to the players
-     * Initiates process for players to distribute troops throughout their given territories
-     */
-    // NEW DOCUMENTATION FOR makePlayers()
     /**
      * sitting the playerNames by getting array of strings from the view and
      * initialize each player with it's playerName
@@ -418,14 +250,6 @@ public class Game {
     }
 
 
-    /**
-     * Creates a new game object and starts the game
-     *
-     * @param args Formality
-     */
-    public static void main(String[] args) {
-        Game game = new Game();
 
-    }
 
 }
