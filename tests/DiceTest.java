38cc29299618fbd4b80f314fc78e5ab3874eb790
Joseph-ctrl7 <72221329+Joseph-ctrl7@users.noreply.github.com>
2020-11-08 22:49:28 -0500

    Update DiceTest.java

--- tests/DiceTest.java
+++ tests/DiceTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.*;
 public class DiceTest {
 
     private Dice dice;
-
+    
+    /**
+     *ensures that a dice roll is between 1 and 6
+     */
     @Test
     public void rollDie() {
         dice = new Dice();
