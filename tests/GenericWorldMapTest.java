71d0f20bba43a1f91223c518d90457944100acd2
Joseph-ctrl7 <72221329+Joseph-ctrl7@users.noreply.github.com>
2020-11-08 22:47:11 -0500

    genericWorldMap test completed

---
+++ tests/GenericWorldMapTest.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+public class GenericWorldMapTest {
+
+    GenericWorldMap gwm;
+
+
+    /**
+     * ensures all territories appear on the map by checking the number
+     * of territories currently on the map
+     */
+    @Test
+    public void getAllTerritories() {
+        gwm = new GenericWorldMap();
+        assertEquals(42, gwm.getAllTerritories().size());
+    }
+
+
+    /**
+     * ensures all territories gotten are properly converted to strings
+     */
+    @Test
+    public void getAllTerritoriesString() {
+        gwm = new GenericWorldMap();
+        LinkedList<Territory> territories = gwm.getAllTerritories();
+        assertTrue(gwm.getAllTerritoriesString().contains("Alaska"));
+        assertTrue(gwm.getAllTerritoriesString().contains("Congo"));
+        assertTrue(gwm.getAllTerritoriesString().contains("Egypt"));
+        assertTrue(gwm.getAllTerritoriesString().contains("Ontario"));
+    }
+
+
+    /**
+     * ensures that a specified territory is properly retrieved
+     */
+    @Test
+    public void getTerritory() {
+        gwm = new GenericWorldMap();
+        Territory ter = new Territory.Builder("Alaska").setContinentName("NorthAmerica")
+                .setNumTerritoriesInContinent(9).setContinentControlBonus(5).build();
+        assertTrue(gwm.getAllTerritories().contains(gwm.getTerritory("Alaska")));
+    }
+}
