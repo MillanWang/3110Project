8bb221df0a2810df60f2204d1e228cc2393aa1e2
Millan <real.millan.wang@gmail.com>
2020-12-08 18:25:10 -0500

    Changed order of stuff in gameView

--- src/GameView.java
+++ src/GameView.java
@@ -26,7 +26,7 @@ public class GameView extends JFrame implements GameObserver{
         gamePanel = new JPanel(new BorderLayout());
         add(gamePanel);
         displayGame();
-        setSize(800, 580);
+        setSize(1000, 580);
         // i changed resizable to true just in case the player wants it full screen
         setResizable(false);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -76,8 +76,8 @@ public class GameView extends JFrame implements GameObserver{
         gamePanel.setVisible(true);
         addMenuItems();
         addMapPicture();
-        settingNumberOfPlayer();
         addMapInfo();
+        settingNumberOfPlayer();
         addGameStatus();
     }
 
